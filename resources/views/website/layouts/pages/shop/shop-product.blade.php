8f970bcaa4d4ee10d7634cd825fd9d39ae8070cd
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-01 11:57:03 +0600

    Products are displayed in a list view fetched from the database.

--- resources/views/website/layouts/pages/shop/shop-product.blade.php
+++ resources/views/website/layouts/pages/shop/shop-product.blade.php
@@ -63,97 +63,8 @@
                 </div>
                 <div class="tab-pane fade" id="list_view" role="tabpanel">
                     <div class="row">
-                        <div class="col-12 mb-40">
-                            <div class="row">
-                                <div class="col-lg-4">
-                                    <div class="single_Product_list">
-                                        <div class="single__product">
-                                            <span class="pro_badge">Sale</span>
-                                            <div class="produc_thumb">
-                                                <a href="product-details.html"><img
-                                                        src="{{ asset('frontend') }}/assets/img/product/1.png"
-                                                        alt=""></a>
-                                            </div>
-                                            <div class="product_hover">
-                                                <div class="product_action">
-                                                    <a href="#" title="Add To Cart"><i
-                                                            class="zmdi zmdi-shopping-cart-plus"></i>
-                                                        Add
-                                                        To Cart</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-8">
-                                    <div class="product__desc">
-                                        <h3><a href="product-details.html">Soffer Pro x33</a></h3>
-                                        <div class="product_rating">
-                                            <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                            <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                            <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                            <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                            <a href="#"><i class="zmdi zmdi-star-half"></i></a>
-                                        </div>
-                                        <div class="price_amount">
-                                            <span class="current_price">$2999.99</span>
-                                            <span class="discount_price">-08%</span>
-                                            <span class="old_price">$3700.00</span>
-                                        </div>
-                                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing
-                                            elit. Est sit cum quaerat accusantium sint fugit
-                                            perspiciatis fugiat cupiditate quia officiis obcaecati
-                                            nesciunt molestias culpa, temporibus natus similique.
-                                            Accusantium, asperiores quia blanditiis nemo repellat ab
-                                            quod similique animi deserunt! Atque, nobis. </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 mb-40">
-                            <div class="row">
-                                <div class="col-lg-4 ">
-                                    <div class="single_Product_list">
-                                        <div class="single__product">
-                                            <div class="produc_thumb">
-                                                <a href="product-details.html"><img
-                                                        src="{{ asset('frontend') }}/assets/img/product/3.png"
-                                                        alt=""></a>
-                                            </div>
-                                            <div class="product_hover">
-                                                <div class="product_action">
-                                                    <a href="#" title="Add To Cart"><i
-                                                            class="zmdi zmdi-shopping-cart-plus"></i>
-                                                        Add
-                                                        To Cart</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-8">
-                                    <div class="product__desc">
-                                        <h3><a href="product-details.html">Soffer Pro x33</a></h3>
-                                        <div class="product_rating">
-                                            <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                            <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                            <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                            <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                            <a href="#"><i class="zmdi zmdi-star-half"></i></a>
-                                        </div>
-                                        <div class="price_amount">
-                                            <span class="current_price">$2999.99</span>
-                                        </div>
-                                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing
-                                            elit. Est sit cum quaerat accusantium sint fugit
-                                            perspiciatis fugiat cupiditate quia officiis obcaecati
-                                            nesciunt molestias culpa, temporibus natus similique.
-                                            Accusantium, asperiores quia blanditiis nemo repellat ab
-                                            quod similique animi deserunt! Atque, nobis. </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        
+                        @forelse ($products as $product)
                         <div class="col-12 mb-40">
                             <div class="row">
                                 <div class="col-lg-4">
@@ -161,8 +72,8 @@
                                         <div class="single__product">
                                             <span class="pro_badge">New</span>
                                             <div class="produc_thumb">
-                                                <a href="product-details.html"><img
-                                                        src="{{ asset('frontend') }}/assets/img/product/5.png"
+                                                <a href="{{ route('product_single.page', $product->id) }}"><img
+                                                        src="{{ asset($product->thumbnail) }}"
                                                         alt=""></a>
                                             </div>
                                             <div class="product_hover">
@@ -178,7 +89,7 @@
                                 </div>
                                 <div class="col-lg-8">
                                     <div class="product__desc">
-                                        <h3><a href="product-details.html">Soffer Pro x33</a></h3>
+                                        <h3><a href="{{ route('product_single.page', $product->id) }}">{{ $product->product_name }}</a></h3>
                                         <div class="product_rating">
                                             <a href="#"><i class="zmdi zmdi-star"></i></a>
                                             <a href="#"><i class="zmdi zmdi-star"></i></a>
@@ -187,65 +98,40 @@
                                             <a href="#"><i class="zmdi zmdi-star-half"></i></a>
                                         </div>
                                         <div class="price_amount">
-                                            <span class="current_price">$2999.99</span>
+                                            @if ($product->discount_price && $product->discount_type === 'flat')
+                                                    @php
+                                                        $product_discount_price =
+                                                            $product->regular_price - $product->discount_price;
+                                                    @endphp
+                                                    <span
+                                                        class="current_price">৳{{ number_format($product_discount_price, 2) }}</span>
+                                                    <span
+                                                        class="old_price">৳{{ number_format($product->regular_price, 2) }}</span>
+                                                @elseif ($product->discount_price && $product->discount_type === 'percent')
+                                                    @php
+                                                        $discount_amount =
+                                                            ($product->regular_price * $product->discount_price) / 100;
+                                                        $product_discount_price =
+                                                            $product->regular_price - $discount_amount;
+                                                    @endphp
+                                                    <span
+                                                        class="current_price">৳{{ number_format($product_discount_price, 2) }}</span>
+                                                    <span class="discount_price">-{{ $product->discount_price }}%</span>
+                                                    <span
+                                                        class="old_price">৳{{ number_format($product->regular_price, 2) }}</span>
+                                                @else
+                                                    <span
+                                                        class="current_price">৳{{ number_format($product->regular_price, 2) }}</span>
+                                                @endif
                                         </div>
-                                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing
-                                            elit. Est sit cum quaerat accusantium sint fugit
-                                            perspiciatis fugiat cupiditate quia officiis obcaecati
-                                            nesciunt molestias culpa, temporibus natus similique.
-                                            Accusantium, asperiores quia blanditiis nemo repellat ab
-                                            quod similique animi deserunt! Atque, nobis. </p>
+                                        <p>{!! $product->short_description !!}</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12">
-                            <div class="row">
-                                <div class="col-lg-4">
-                                    <div class="single_Product_list">
-                                        <div class="single__product">
-                                            <span class="pro_badge">Sale</span>
-                                            <div class="produc_thumb">
-                                                <a href="#"><img
-                                                        src="{{ asset('frontend') }}/assets/img/product/home2/4.png"
-                                                        alt=""></a>
-                                            </div>
-                                            <div class="product_hover">
-                                                <div class="product_action">
-                                                    <a href="#" title="Add To Cart"><i
-                                                            class="zmdi zmdi-shopping-cart-plus"></i>
-                                                        Add
-                                                        To Cart</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-8">
-                                    <div class="product__desc">
-                                        <h3><a href="product-details.html">Soffer Pro x33</a></h3>
-                                        <div class="product_rating">
-                                            <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                            <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                            <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                            <a href="#"><i class="zmdi zmdi-star"></i></a>
-                                            <a href="#"><i class="zmdi zmdi-star-half"></i></a>
-                                        </div>
-                                        <div class="price_amount">
-                                            <span class="current_price">$2999.99</span>
-                                            <span class="discount_price">-08%</span>
-                                            <span class="old_price">$3700.00</span>
-                                        </div>
-                                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing
-                                            elit. Est sit cum quaerat accusantium sint fugit
-                                            perspiciatis fugiat cupiditate quia officiis obcaecati
-                                            nesciunt molestias culpa, temporibus natus similique.
-                                            Accusantium, asperiores quia blanditiis nemo repellat ab
-                                            quod similique animi deserunt! Atque, nobis. </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        @empty
+                        <p>Product not found.</p>
+                        @endforelse
                     </div>
                 </div>
             </div>
